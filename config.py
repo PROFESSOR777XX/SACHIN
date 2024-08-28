eac07b97bbdb8870c13a0cb52d7610cb33fb7fe5
PROFESSOR777XX <professor7711x@gmail.com>
2024-08-28 10:14:21 +0530

    ❖ | sᴧᴄʜɪɴ ꭙ sᴧɴᴧᴛᴧɴɪ | ❖

--- config.py
+++ config.py
@@ -33,7 +33,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/ERA990/MSIC",
+    "https://github.com/PROFESSOR777XX/SACHIN",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
